29a846298243a83238664df4e4b8ae2be7fa9315
Anthony Zavadil <anthony.zavadil@gmail.com>
2013-02-07 09:00:01 -0800

    Final version of hw3

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -22,14 +22,15 @@ Background: movies have been added to database
   And  I am on the RottenPotatoes home page
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
+  Given I check the following ratings: PG, R
+  Then I uncheck the following ratings: G, PG-13, NC-17
+  And I press "Refresh" 
+  Then I should see movies with the following ratings: PG, R
+  And I should not see movies with the following ratings: G, PG-13, NC-17
 
-Scenario: no ratings selected
-  # see assignment
 
 Scenario: all ratings selected
-  # see assignment
+  When I check the following ratings: G, PG, PG-13, R, NC-17
+  And I press "Refresh" 
+  Then I should see all of the movies
+  
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!( movie )
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,15 +14,70 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #flunk
+  regexp = /#{e1}.*#{e2}/m
+  page.body.should =~ regexp
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
+
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.gsub!(","," ").split.each do |rating|
+    if uncheck
+      step %Q{I uncheck "ratings_#{rating}"}
+    else
+      step %Q{I check "ratings_#{rating}"}
+    end
+  end
 end
+
+When /^I should see movies with the following ratings: (.*)/ do |rating_list|
+  ##puts rating_list
+  ##puts rating_list.split(",")
+  Movie.find_all_by_rating( rating_list.gsub!(","," ").split ).each do |movie|
+    step %Q{I should see "#{movie.title}"}
+  end
+end
+
+When /^I should not see movies with the following ratings: (.*)/ do |rating_list|
+  Movie.find_all_by_rating( rating_list.gsub!(","," ").split ).each do |movie|
+    step %Q{I should not see "#{movie.title}"}
+  end
+end
+
+When /^I check all of the ratings/ do
+  Movie.all_ratings.each do |rating|
+    check("ratings_#{rating}")
+  end
+end
+
+When /^I uncheck all of the ratings/ do
+  Movie.all_ratings.each do |rating|
+    uncheck("ratings_#{rating}")
+
+    ## alternative: call web_steps
+    #step %Q{I uncheck "ratings_#{rating}"}
+  end
+end
+
+
+When /^I should see all of the movies/ do
+  ## Count the number of rows in the table.
+  ## e.g. rows.should == value
+
+  movie_count = Movie.all.count
+  rows = page.all('table#movies tr').count
+  rows.should == movie_count+1  ## +1 to account for headers
+end
+
+When /^I should see 5 movies/ do
+  rows = page.all('table#movies tr').count
+  rows.should == 7
+end
+
